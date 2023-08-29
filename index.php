8ec96b50c4c0e71453e970da195b464a90d26ad3
swapnilkhandare27 <khandareswapnil539@gmail.com>
2023-08-29 19:55:32 +0530

    crudfeatures

---
+++ index.php
@@ -0,0 +1,285 @@
+<?php
+
+//connecting to database
+
+$insert = false;
+$update = false;
+//$delete = false;
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "notes";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+  die("Connection failed to database");
+}
+
+if (isset($_POST['delete'])) {
+  $sno = $_POST['delete'];
+  $delete = true;
+  $sql="DELETE FROM `note` WHERE `note`.`sr` =$sno";
+  $result = mysqli_query($conn, $sql);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  
+
+  if (isset($_POST['snoEdit'])) {
+    // Update the record
+    $sno = $_POST["snoEdit"];
+    $title = $_POST["titleEdit"];
+    $description = $_POST["descriptionEdit"];
+
+    // Sql query to be executed
+    $sql = "UPDATE `note` SET `title` = '$title' , `description` = '$description' WHERE `note`.`sr` = $sno";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+      $update = true;
+    } else {
+      echo "We could not update the record successfully";
+    }
+  } else {
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+
+    // Sql query to be executed
+    $sql = "INSERT INTO `note` (`title`, `description`) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $sql);
+
+
+    if ($result) {
+      $insert = true;
+    } else {
+      echo "The record was not inserted successfully because of this error ---> " . mysqli_error($conn);
+    }
+  }
+}
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+
+  <style>
+    #one {
+      border-bottom: 5px solid black;
+      padding-bottom: 20px;
+    }
+
+    legend {
+      border-bottom: 5px solid red;
+
+    }
+  </style>
+
+  <title>Inotes</title>
+</head>
+
+<body>
+
+
+  <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editModalLabel">Edit this Note</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="/miniproject/index.php" method="POST">
+          <div class="modal-body">
+            <input type="hidden" name="snoEdit" id="snoEdit">
+            <div class="form-group">
+              <label for="title">Note Title</label>
+              <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+            </div>
+
+            <div class="form-group">
+              <label for="desc">Note Description</label>
+              <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+            </div>
+          </div>
+          <div class="modal-footer d-block mr-auto">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Inotes</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <?php
+  if ($insert) {
+    echo    "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+  <strong>Success!</strong> You note has been inserted successfully.
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+</div>";
+  }
+  if ($update) {
+    echo   "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+  <strong>Updated !</strong> Your record has been updated successfully.
+  <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+</div>";
+  }
+
+
+  ?>
+  <div id="one" class="container my-4 ">
+    <form action="/miniproject/index.php" method="POST">
+      <fieldset>
+        <legend>
+          <h2>Add a Note to iNotes App</h2>
+        </legend>
+        <div class="mb-3">
+          <label for="title" class="form-label">Note Title</label>
+          <input type="text" id="title" class="form-control" name="title" placeholder="Enter Title">
+        </div>
+
+        <div class="form-group">
+          <label for="desc">Note Description</label>
+          <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-2">Add Note</button>
+      </fieldset>
+    </form>
+  </div>
+  <br>
+  <div class="container mt-5 my-4 ">
+
+    <table class="table" id="myTable">
+      <thead>
+        <tr>
+          <th scope="col">Sr.no</th>
+          <th scope="col">Title</th>
+          <th scope="col">Description</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "SELECT * FROM `note`";
+        $result = mysqli_query($conn, $sql);
+        $sno = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+          $sno = $sno + 1;
+          echo " <tr>
+             <th scope='row'>" . $sno . "</th>
+             <td>" . $row['title'] . "</td>
+             <td>" . $row['description'] . "</td>
+             <td> <button class='edit btn btn-sm btn-primary' id=" . $row['sr'] . ">Edit</button> <button class='delete btn btn-sm btn-primary' id='d' " . $row['sr'] . ">Delete</button>  </td>
+             </tr>";
+        }
+        ?>
+
+      </tbody>
+    </table>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#myTable').DataTable();
+    });
+  </script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+
+
+  <script>
+    //For updating the records
+
+    edits = document.getElementsByClassName('edit');
+    Array.from(edits).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("edit ");
+        tr = e.target.parentNode.parentNode;
+        title = tr.getElementsByTagName("td")[0].innerText;
+        description = tr.getElementsByTagName("td")[1].innerText;
+        console.log(title, description);
+        titleEdit.value = title;
+        descriptionEdit.value = description;
+        snoEdit.value = e.target.id;
+        console.log(e.target.id)
+        $('#editModal').modal('toggle');
+      })
+    })
+
+    //For deleting the records
+    deletes = document.getElementsByClassName('delete');
+    Array.from(deletes).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("edit ");
+        sno = e.target.id.substr(1);
+
+        if (confirm("Are you sure you want to delete this note!")) {
+          console.log("yes");
+          window.location = `/miniproject/index.php?delete=${sno}`;
+          // TODO: Create a form and use post request to submit a form
+        } else {
+          console.log("no");
+        }
+      })
+    })
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
